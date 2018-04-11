ab6698f0b36ac37d458879d2af9b3ef55d24ee13
aaron2612 <33220526+aaron2612@users.noreply.github.com>
2018-04-11 15:56:19 +0800

    1、实现comfirm弹窗componet 2、实现“确定”按钮component, 3、实现底部Tab的component 4、实现“首页”静态页面和“我的兆方卡”静态页面开发

--- wxxiao/pages/index/index.js
+++ wxxiao/pages/index/index.js
@@ -4,51 +4,25 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    listArr: [
+      {name:'兆方车上付',notice:'一键加油，更省心',iconUrl: '../../images/home_icon_zfpay.png',link: '../../pages/logs/logs'},
+      {name:'我的兆方卡',notice:'一键充值，享更高优惠',iconUrl: '../../images/home_icon_zfcar.png',link: '../../pages/carType/carType'},
+      {name:'交易查询',notice:'实时交易查询，更便捷',iconUrl: '../../images/home_icon_transaction.png',link: ''},
+      {name:'积分商城',notice:'积分兑好礼，优惠享不停',iconUrl: '../../images/home_icon_integral.png',link: ''}
+    ]
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
+
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+
+  onReady: function () {
+
+  },
+  goNext(event) {
+    wx.navigateTo({
+      url: event.currentTarget.dataset.link
     })
-  }
+  },
+ 
 })
---
+++ wxxiao/pages/myCard/myCard.js
@@ -0,0 +1,69 @@
+// pages/myCard/myCard.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    listArr: [
+      {name:'单位卡',notice:'持有单位分配的加油卡用户请选择',iconUrl: '../../images/home_icon_zfcar.png',link: '../../pages/logs/logs'},
+      {name:'个人卡',notice:'个人办理的加油卡用户请选择',iconUrl: '../../images/home_icon_zfcar.png',link: '../../pages/carType/carType'},
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
