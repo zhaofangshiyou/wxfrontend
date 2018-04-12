adb1145aacb5e6099f3edb0613582dd338bdb430
aaron2612 <33220526+aaron2612@users.noreply.github.com>
2018-04-12 17:01:12 +0800

    “取票油站”静态页面开发完成

---
+++ wxxiao/pages/oilStation/oilStation.js
@@ -0,0 +1,70 @@
+// pages/oilStation/oilStation.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    oilArr: [
+      {name:'华富加油站',address:'安徽省铜陵市铜官区滨江大道中段'},
+      {name:'雨坛加油站',address:'安徽省枞阳县雨坛乡雨坛村义小公路西侧'},
+      {name:'广阳加油站',address:'安徽省黄山市太平湖朝阳村'}
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
