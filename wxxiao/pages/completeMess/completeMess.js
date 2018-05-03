bcfdf991931e791a2f96889e7aee64b7848c383c
aaron2612 <33220526+aaron2612@users.noreply.github.com>
2018-05-03 17:44:36 +0800

    完善资料接口对接

--- wxxiao/pages/completeMess/completeMess.js
+++ wxxiao/pages/completeMess/completeMess.js
@@ -1,4 +1,6 @@
 // pages/completeMess/completeMess.js
+import httpService from '../../http/http.js';
+const app = getApp();
 Page({
 
   /**
@@ -7,7 +9,78 @@ Page({
   data: {
     chooseSex: 1,
     carTypeBrand: '',
-    showCar: false
+    showCar: false,
+    realName: '',
+    IDcard: '',
+    carNum: ''
+  },
+   /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    this.getUserInfo();
+  },
+  //H获取用户信息
+  getUserInfo: function() {
+    httpService.sendRrquest(app.config.host+'/user',{userId: wx.getStorageSync('user_id')},{},'GET')
+      .then(res => {
+        if(res.data.status === 0) {
+          var user = res.data.data.user;
+          if(user.sex) {
+            this.setData({
+              chooseSex: user.sex
+            })
+          }
+          this.setData({
+            realName: user.name,
+            IDcard: user.id_card,
+            carTypeBrand: user.car_type,
+            carNum: user.car_num
+          })
+        }
+      })
+  },
+  submitComplete: function() {
+    var that = this;
+    if(this.data.IDcard) {
+      if(!this.ValidatID(this.data.IDcard)) {
+        return false;
+      }
+    }else if(this.data.carNum) {
+      if(!this.validaCar(this.data.carNum)) {
+          return false;
+      }
+    }
+    httpService.sendRrquest(app.config.host+'/user',{userId: wx.getStorageSync('user_id')},{
+      name: that.data.realName,
+      sex: that.data.chooseSex,
+      id_card: that.data.IDcard,
+      car_num: that.data.carNum,
+      car_type: that.data.carTypeBrand
+    },'PUT').then( res => {
+      if(res.data.status === 0) {
+        this.getUserInfo();
+      }
+    })
+  },
+  checkMsg: function(event) {
+    switch(event.currentTarget.dataset.flag) {
+      case 'realName': 
+        this.data.realName = event.detail.value;
+        break;
+      case 'ID':
+        if(!this.ValidatID(event.detail.value)) {
+          this.warnMsg('请输入正确的身份证号码！');
+        }
+        this.data.IDcard = event.detail.value;
+        break;
+      case 'carNum':
+        if(!this.validaCar(event.detail.value)) {
+          this.warnMsg('请输入正确的车牌号！');
+        }
+        this.data.carNum = event.detail.value;
+        break;
+    }
   },
   changeSex: function(e) {
     if(e.currentTarget.dataset.sex==1){
@@ -26,6 +99,33 @@ Page({
     })
   },
 
+  //验证身份证
+  ValidatID(id){
+    let patten = /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/;
+    if(patten.test(id)){
+      return true;
+    }else{
+      return false;
+    }
+  },
+  //验证车牌号
+  validaCar(carNumber) {
+    let patten = /^[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领A-Z]{1}[A-Z]{1}[A-Z0-9]{4}[A-Z0-9挂学警港澳]{1}$/;
+    if(patten.test(carNumber)){
+      return true;
+    }else{
+      return false;
+    }
+  },
+  warnMsg: function(text) {
+    wx.showToast({
+      title: text,
+      icon: 'none',
+      duration: 2000,
+      mask:true
+  })
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
@@ -33,12 +133,7 @@ Page({
   
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
+ 
 
   /**
    * 生命周期函数--监听页面显示
