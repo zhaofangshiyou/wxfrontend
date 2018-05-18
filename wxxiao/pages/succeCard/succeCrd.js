ab1134110a9fbc6391ee4b935deffd99e6b9f0be
aaron2612 <33220526+aaron2612@users.noreply.github.com>
2018-05-18 09:58:04 +0800

    小程序-“卡详情”更改获取所需要字段的获取方法、拼接卡号功能

--- wxxiao/pages/succeCard/succeCrd.js
+++ wxxiao/pages/succeCard/succeCrd.js
@@ -1,13 +1,19 @@
 // pages/succeCard/succeCrd.js
+import httpService from '../../http/http.js';
+
+const app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    balance: 0,
     title: '',
-    type: ''
+    type: '',
+    card_num: '',
+    haveCard: true,
+    haveMenuCard: true,
+    balance: 0,
   },
   finishSubmi: function() {
     wx.reLaunch({
@@ -20,6 +26,41 @@ Page({
     })
   },
 
+    //获取卡信息
+    getCardInfo: function() {
+      var that = this;
+      httpService.sendRrquest(app.config.host+'/card',{userId: wx.getStorageSync('user_id')},{},'GET')
+        .then(res => {
+          if(res.data.status === 0) {
+            if(this.data.type == 1 || this.data.type == 6 || this.data.type == 0) {
+              var temp_number = this.joinCard(res.data.data.card.card_prefix,res.data.data.card.id.toString());
+              that.setData({
+                card_num: temp_number,
+                balance: parseInt(res.data.data.card.person_balance)
+              })
+            }else if(this.data.type == 2) {
+              that.setData({
+                card_num: '1001 8888',
+                balance: parseInt(res.data.data.card.company_balance)
+              })
+            }
+          }
+        })
+    },
+  
+    //拼接卡号
+    joinCard: function(prex,id) {
+      let str_id = id.toString();
+      let id_len = str_id.length;
+      let str_0 = '';
+      for(let i=0; i< 8-id_len; i++) {
+        str_0 = str_0 + '0';
+      }
+      let new_num = prex +'  ' + str_0 + str_id;
+      return new_num;
+    },
+
+
   /**
    * 生命周期函数--监听页面加载
    */
@@ -27,9 +68,10 @@ Page({
     console.log(res);
     var that = this;
     that.setData({
-      balance: parseInt(res.balance),
+      // balance: parseInt(res.balance),
       title: res.title,
-      type: res.type
+      type: res.type,
+      // card_num: res.card_number
     })
   },
 
@@ -37,7 +79,7 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+    this.getCardInfo();
   },
 
   /**
