6254f54c4767c292f999fc0223af1ca586b64857
aaron2612 <33220526+aaron2612@users.noreply.github.com>
2018-05-30 16:08:29 +0800

    后台管理系统优惠信息文案界面优化

--- zhaofang-admin/src/views/nav3/discountUE.vue
+++ zhaofang-admin/src/views/nav3/discountUE.vue
@@ -1,10 +1,9 @@
 <template>
   <div class="components-container">
-    <div class="info">UE编辑器示例<br>需要使用编辑器时，调用UE公共组件即可。可设置填充内容defaultMsg，配置信息config(宽度和高度等)，可调用组件中获取内容的方法。</div>
-    <button @click="getUEContent()">获取内容</button>
     <div class="editor-container">
       <UE :defaultMsg=defaultMsg :config=config ref="ue"></UE>
     </div>
+     <button @click="getUEContent()" class="submit">提交</button>
   </div>
 </template>
 <style>
@@ -14,6 +13,16 @@
     padding: 10px;
     margin: 10px;
     background-color: #ffffff;
+  }
+  .submit{
+    margin-top: 20px;
+    text-align: center;
+    width: 100px;
+    height: 40px;
+    background: #20a0ff;
+    color: #fff;
+    border: 1px solid #20a0ff;
+    border-radius: 4px;
   }
 </style>
 <script>
