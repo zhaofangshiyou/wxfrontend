086c33a8e314f69960dd3ecdeac41417da63cabd
aaron2612 <33220526+aaron2612@users.noreply.github.com>
2018-07-04 15:06:42 +0800

    后台管理系统登录页面UI优化

--- zhaofang-admin/src/views/Login.vue
+++ zhaofang-admin/src/views/Login.vue
@@ -1,18 +1,29 @@
 <template>
-  <el-form :model="ruleForm2" :rules="rules2" ref="ruleForm2" label-position="left" label-width="0px" class="demo-ruleForm login-container">
-    <h3 class="title">系统登录</h3>
-    <el-form-item prop="account">
-      <el-input type="text" v-model="ruleForm2.account" auto-complete="off" placeholder="账号"></el-input>
-    </el-form-item>
-    <el-form-item prop="checkPass">
-      <el-input type="password" v-model="ruleForm2.checkPass" auto-complete="off" placeholder="密码"></el-input>
-    </el-form-item>
-    <!-- <el-checkbox v-model="checked" checked class="remember">记住密码</el-checkbox> -->
-    <el-form-item style="width:100%;">
-      <el-button type="primary" style="width:100%;" @click.native.prevent="handleSubmit2" :loading="logining">登录</el-button>
-      <!--<el-button @click.native.prevent="handleReset2">重置</el-button>-->
-    </el-form-item>
-  </el-form>
+    <div class="login_bg">
+      <div class="login_title">
+        <img class="logo" width="100%" src="../assets/logo_title.png" align="absmiddle" />
+      </div>
+      <el-form :model="ruleForm2" :rules="rules2" ref="ruleForm2" label-position="left" label-width="0px" class="demo-ruleForm login-container">
+        <h3 class="title">用户登录</h3>
+        <div class="welcome_box">
+          <div class="tip"></div>
+          <div class="welcome_text">欢迎登录兆方后台管理系统</div>
+          <div class="tip"></div>
+        </div>
+        <el-form-item prop="account">
+          <el-input type="text" v-model="ruleForm2.account" auto-complete="off" placeholder="账号"></el-input>
+        </el-form-item>
+        <el-form-item prop="checkPass">
+          <el-input type="password" v-model="ruleForm2.checkPass" auto-complete="off" placeholder="密码"></el-input>
+        </el-form-item>
+        <!-- <el-checkbox v-model="checked" checked class="remember">记住密码</el-checkbox> -->
+        <el-form-item style="width:100%;">
+          <el-button type="primary" style="width:100%;" @click.native.prevent="handleSubmit2" :loading="logining">登录</el-button>
+          <!--<el-button @click.native.prevent="handleReset2">重置</el-button>-->
+        </el-form-item>
+      </el-form>
+      <div class="login_bottom">Copyright © 2012-2018 深圳市兆方石油化工股份有限公司 版权所有</div>
+    </div>
 </template>
 
 <script>
@@ -93,25 +104,64 @@
 </script>
 
 <style lang="scss" scoped>
+  .login_bg{
+    background: url('../assets/bg.png') no-repeat;
+    background-size: 100% auto;
+    position: absolute;
+    width: 100%;
+    .login_bottom{
+      margin-top: 120px;
+      text-align: center;
+      color: #949494;
+    }
+    .login_title{
+      margin: 250px 0 auto;
+      text-align: center;
+      padding: 0;
+      line-height: 0;
+      height: 42px;
+      .logo{
+        display: inline-block;
+        width: auto;
+        height: 42px;
+        line-height: 42px;
+      }
+    }
+  }
   .login-container {
     /*box-shadow: 0 0px 8px 0 rgba(0, 0, 0, 0.06), 0 1px 0px 0 rgba(0, 0, 0, 0.02);*/
     -webkit-border-radius: 5px;
     border-radius: 5px;
     -moz-border-radius: 5px;
     background-clip: padding-box;
-    margin: 180px auto;
+    margin: 50px auto;
     width: 350px;
     padding: 35px 35px 15px 35px;
     background: #fff;
     border: 1px solid #eaeaea;
-    box-shadow: 0 0 25px #cac6c6;
-    .title {
+    box-shadow:0px 0px 24px rgba(113,167,242,0.6);
+    .welcome_box{
+      width: 100%;
+      display: flex;
+      height: 28px;
       margin: 0px auto 40px auto;
+      .tip{
+        flex: 1;
+        border-top: 1px solid rgba(189,189,189,1);
+        margin-top: 14px;
+      }
+      .welcome_text{
+        padding: 0 5px;
+        line-height: 28px;
+      }
+    }
+    .title {
       text-align: center;
-      color: #505458;
+      color: #4E9FF8;
     }
     .remember {
       margin: 0px 0px 35px 0px;
     }
   }
+
 </style>
--- zhaofang-admin/src/views/nav2/consumerDetail.vue
+++ zhaofang-admin/src/views/nav2/consumerDetail.vue
@@ -85,7 +85,9 @@
 			</el-table-column>
 			<el-table-column prop="province_name" label="省份">
 			</el-table-column>
-			<el-table-column prop="station_name" label="站点名称">
+			<el-table-column prop="station_name" label="站点名称" width="180">
+			</el-table-column>
+      <el-table-column prop="cc_flow_id" label="流水号">
 			</el-table-column>
 			<el-table-column prop="card_no" label="卡号" width="180">
 			</el-table-column>
